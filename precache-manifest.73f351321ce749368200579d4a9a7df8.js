f990798d7650d220802f7e1cfbad33a98acc767b
Aldrian <himalosc@gmail.com>
2020-02-17 14:10:10 +0800

    Updates

---
+++ precache-manifest.73f351321ce749368200579d4a9a7df8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8718909f8d0ca28c35990b8600daea57",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "509005393994b30e3c69",
+    "url": "/Robofriends/static/css/main.306e2eb1.chunk.css"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/Robofriends/static/js/2.d6fca685.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robofriends/static/js/2.d6fca685.chunk.js.LICENSE"
+  },
+  {
+    "revision": "509005393994b30e3c69",
+    "url": "/Robofriends/static/js/main.76f16e4e.chunk.js"
+  },
+  {
+    "revision": "9eff52c4d1627d87e058",
+    "url": "/Robofriends/static/js/runtime-main.b221b423.js"
+  },
+  {
+    "revision": "b2749db844dcf708cf6434501dea8aa7",
+    "url": "/Robofriends/static/media/btseps2.b2749db8.woff"
+  }
+]);
